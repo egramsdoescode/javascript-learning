abac7b5120510b2795c7bee7707114dbd5efd394
ethandoes-code <ethandoescode@gmail.com>
2025-09-09 08:05:49 -0500

    (study): Start 12-Promises/09-async

---
+++ 12-Promises/09-async.js
@@ -0,0 +1,15 @@
+async function getMessageHash(sender, content) {
+    const hash = await sha256Hex(content);
+    return `Sender (${sender}): ${hash}`;
+}
+
+async function sha256Hex(str) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(str);
+    const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
+    return hex;
+}
+
+console.log(getMessageHash("Ethan", "This is an encrypted message."));
